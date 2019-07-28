66eb13c0498a54b85b771dbc19471d4c0da9d202
Ferran <fbarneda@gmail.com>
2019-07-28 22:44:54 +0100

    Just Committing

--- export_list_of_agents.py
+++ export_list_of_agents.py
@@ -1,94 +1,135 @@
 """
 * Please edit "subdomain" with your subdomain
-
 * Please edit "username" with your Zendesk email address. Do not remove '/token'
-
 * Please edit "token" with a valid token from Admin > API > Token Access
-
 """
 
-import requests, json, time
+import requests, json, time, math
+
+
+def reportDetails(r,page,num_users_to_export):
+    page += 1
+    print("[HTTP " + str(r.status_code) + "] - " + "Working on page number " + str(page) + "/" + str(math.ceil(num_users_to_export/100)), end='')
+    print(" - RPM " + str(r.headers['X-Rate-Limit-Remaining']) + "/" + str(r.headers['X-Rate-Limit']))
 
 
 def letsWait(wait_seconds):
-
-    count_down = list(range(1,wait_seconds+1))
+    count_down = list(range(1, wait_seconds + 1))
     count_down.reverse()
 
     for num in count_down:
-
         print(str(num) + " seconds left")
         time.sleep(1)
 
 
-subdomain = 'XXXXX'
-user = 'XXXX@XXX.XXX/token'
-token = 'XXXXX'
+subdomain = 'xxxxx'
+username = 'xxxxx@xxx.xxx/token'
+token = 'xxxxxxx'
 file_name = 'agents.csv'
 
-url = 'https://' + subdomain + '.zendesk.com/api/v2/users.json?role[]=agent&role[]=admin'
-print('\n>>> EXPORTING from: ',url,'\n')
+# If you’re making several requests to the same host, the underlying TCP connection will be reused,
+# which can result in a significant performance increase
+session = requests.Session()
 
-# Create file
+# We will be using the Tickets end-point
+# For more info: https://developer.zendesk.com/rest_api/docs/support/search
+url = 'https://' + subdomain + '.zendesk.com/api/v2/users.json?role[]=agent&role[]=admin'
+
+print(f"\nEXPORT ALL MY AGENTS"
+      f"\n**********************\n"
+      f"\n* SUBDOMAIN: {subdomain}"
+      f"\n* USER: {username}"
+      f"\n* TOKEN: {token}"
+      f"\n\n>>> Starting")
+
+# Create file, adding a head line
 my_file = open(file_name, 'w+')
 my_file.write('"ID","NAME","EMAIL","ROLE","CREATED_AT","UPDATED_AT"\n')
 
 page = 0
-
+num_users_to_export = 0
 while url:
 
-    r = requests.get(url, auth=(user, token))
+    r = session.get(url, auth=(username, token))
 
-# HTTP 200 Range
+    # HTTP 200 Range
     if str(r.status_code)[0] == '2':
-
-        page += 1
-
-        print("Working on page number " + str(page))
 
         json_data = r.json()
 
         for user in json_data['users']:
-
-            print(json.dumps(user))
-
+            
             my_file.write(json.dumps(user['id']) + ", ")
             my_file.write(json.dumps(user['name']) + ", ")
             my_file.write(json.dumps(user['email']) + ", ")
             my_file.write(json.dumps(user['role']) + ", ")
             my_file.write(json.dumps(user['created_at']) + ", ")
             my_file.write(json.dumps(user['updated_at']) + "\n")
-
+            
+        num_users_to_export = json_data['count']
         url = json_data['next_page']
 
+        # Printing details about the export, like number of pages left or API requests per minute used/left
+        reportDetails(r,page,num_users_to_export)
 
-# HTTP 400 Range
+        # Check if 'next_page' is None to finish export
+        if url is None:
+
+            print('\n>>> Finished')
+            # Closing the file we are working on
+            my_file.close()
+            # read the file into a list of lines
+            file_lines = open(file_name, 'r').readlines()
+            # remove the ',' at the end of the last line and add a text to the end to make it a valid JSON
+            new_last_line = (file_lines[-1][:-2] + "\n]}")
+            file_lines[-1] = new_last_line
+            #  write the modified last line back to the file
+            open(file_name, 'w').writelines(file_lines)
+
+            print(f"\n>>> All data saved in file: {file_name}")
+            break
+
+        else:
+            continue
+
+
+    # HTTP 400 Range
+    elif str(r.status_code) == '401':
+
+        print("\n>>> HTTP 401 received. Couldn't authenticate you. Please review SUBDOMAIN, USERNAME and TOKEN.")
+        break
+
     elif str(r.status_code) == '403':
 
-        print("HTTP 403 received. A 403 response means the server has determined the user or the account doesn’t have the required permissions to use the API.")
+        print(
+            "\n>>> HTTP 403 received. A 403 response means the server has determined the user or the account doesn’t have the required permissions to use the API.")
         break
 
     elif str(r.status_code) == '409':
 
-        print("HTTP 409 received. A 409 response can indicate a merge conflict, but it often indicates a uniqueness constraint error in our database due to the attempted simultaneous creation of a resource. Try your API call again.\nIn general, the Zendesk API can handle concurrent API requests but the requests shouldn't be talking about the same resources such as the same requester. Serialize requests where possible.")
+        print(
+            "\n>>> HTTP 409 received. A 409 response can indicate a merge conflict, but it often indicates a uniqueness constraint error in our database due to the attempted simultaneous creation of a resource. Try your API call again.\nIn general, the Zendesk API can handle concurrent API requests but the requests shouldn't be talking about the same resources such as the same requester. Serialize requests where possible.")
         break
 
     elif str(r.status_code) == '422':
 
-        print("HTTP 422 received. A 422 response means that the content type and the syntax of the request entity are correct, but the content itself is not processable by the server. This is usually due to the request entity not being relevant to the resource that it's trying to create or update. Example: Trying to close a ticket that's already closed.")
+        print(
+            "\n>>> HTTP 422 received. A 422 response means that the content type and the syntax of the request entity are correct, but the content itself is not processable by the server. This is usually due to the request entity not being relevant to the resource that it's trying to create or update. Example: Trying to close a ticket that's already closed.")
         break
 
 
-# HTTP 429
+    # HTTP 429
+    # Best practices for avoiding rate limiting: https://develop.zendesk.com/hc/en-us/articles/360001074328
     elif str(r.status_code) == '429':
 
-        print("HTTP 429 received. Waiting to retry again.")
+        print("\n>>> HTTP 429 received. Waiting to retry again.")
 
         try:
 
             time_to_wait = int(r.headers['Retry-After'])
-            print("Received a 'Retry-After' of " + str(time_to_wait) + " seconds.")
+            print("Received a 'Retry-After' of " + str(time_to_wait) + " seconds.\n")
             letsWait(time_to_wait)
+            print("\nTrying again:")
             continue
 
         except:
@@ -98,21 +139,17 @@ while url:
             continue
 
 
-# HTTP 500 range
+    # HTTP 500 range
     elif str(r.status_code)[0] == '5':
 
-        print("HTTP 500 range received. Printing request:\n")
+        print("\n>>> HTTP 500 range received. Printing request:\n")
         print(f"\n** HEADERS **\n {r.headers}")
         print(f"\n** CONTENT **\n {r.content}")
         break
 
-
     else:
+        print(f"\n>>> ERROR\n {r.json()}")
+        print(f"{r.status_code}")
+        break
 
-        print("Something went wrong, please review the information entered.")
-
-
-# Closing the file we are working on
-my_file.close()
-
-print("\n>>> DONE - Export Finished OK")
+session.close()
