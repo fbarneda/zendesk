36fe4c72391220d78cd564c5f5188be19827412c
Ferran Barneda <31411719+fbarneda@users.noreply.github.com>
2019-11-06 16:36:28 +0000

    Update and rename convert_export_to_valid_json.py to convert_tickets_export_into_valid_json.py

---
+++ convert_tickets_export_into_valid_json.py
@@ -0,0 +1,25 @@
+"""
+Convert JSON export into a valid JSON file.
+For more info:
+https://support.zendesk.com/hc/en-us/articles/203662346-Exporting-data-to-a-JSON-CSV-or-XML-file-Professional-and-Enterprise-
+"""
+
+def main():
+    
+    input_file = 'tickets.json'
+    output_file = 'export.json'
+
+    with open(input_file, 'r') as all_input_file_lines:
+        all_input_file_lines.readline()
+
+        all_lines_with_comma = str()
+
+        for line in all_input_file_lines:
+            all_lines_with_comma += line[:-1] + ",\n"
+
+    with open(output_file, 'w+') as write_output_file:
+        write_output_file.writelines('{"tickets":[' + '\n' + all_lines_with_comma[:-3] + "\n}]}")
+
+
+if __name__ == "__main__":
+    main()
